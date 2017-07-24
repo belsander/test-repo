eb33a1a016ad67e654fc34b873ada4a09d09f2cc
Sander Bel <sander@proapps.be>
2017-07-24 12:03:39 +0100

    Added metadata file

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name 'test-repo'
+maintainer 'Sander Bel'
+maintainer_email 'sander@proapps.be'
+description 'Test!'
+long_description 'A long test'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+issues_url 'https://github.com/belsander'
+source_url 'https://github.com/belsander'
+supports 'ubuntu'
