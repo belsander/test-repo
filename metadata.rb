415a55169349de20ff9cd8409b103bf66c291442
root <root@default-ubuntu-1604.vagrantup.com>
2017-07-26 11:24:08 +0000

    Auto increased version from 0.1.8 to 0.1.9

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Sander Bel'
 maintainer_email 'sander@proapps.be'
 description 'Test!'
 long_description 'A long test'
-version '0.1.8'
+version '0.1.9'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/belsander'
 source_url 'https://github.com/belsander'
